06af1bf59633c3d901f7290c60ee7189ac7ad7e0
Eliana Ortiz <elianay.hernandezo@uqvirtual.edu.co>
2024-11-21 21:06:27 -0500

    Creación de la clase cotizanteDao

---
+++ src/main/java/CSV/CotizantesDao.java
@@ -0,0 +1,11 @@
+package CSV;
+
+import Model.Cotizante;
+
+import java.io.IOException;
+
+public class CotizantesDao extends ADao<Cotizante, Integer> {
+    public CotizantesDao(String rutaArchivo) throws IOException {
+        super(rutaArchivo);
+    }
+}
